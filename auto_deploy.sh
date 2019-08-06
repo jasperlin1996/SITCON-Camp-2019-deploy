28f7b263f8cb37b1ce72dd855ddbc8325331e7c0
Jasper Lin <jasperlin1996@gmail.com>
2019-08-06 15:45:32 +0800

    Use printf instead of echo

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -7,22 +7,22 @@
 # ./vlc.yml
 
 # Find avaliable ip address
-echo "\n=== Find avaliable ip address ===\n"
-echo "[defaults]\n \
+printf "\n=== Find avaliable ip address ===\n"
+printf "[defaults]\n \
 inventory = ~/ansible-SITCON/hosts_back\n \
 host_key_checking = False\n" > ansible.cfg
 ansible sitcon-camp -m ping > ip.txt
 rm ~/ansible-SITCON/hosts_back
 
 # Split different account (sitcon2019, SITCON2019)
-echo "\n==== Split different account ====\n"
+printf "\n==== Split different account ====\n"
 grep "SUCC" < ip.txt | awk '{print $1}' > sitcon2019_ip_0.txt
 grep -B 2 "Permission" < ip.txt | awk 'NR%4==1{print $1}' > SITCON2019_ip_1.txt
 python3 make_hosts.py
 
 # Auto deploy vlc client stream to rtmp://192.168.3.122/live/livestream
-echo "\n======= Deploy vlc client =======\n"
-echo "[defaults]\n \
+printf "\n======= Deploy vlc client =======\n"
+printf "[defaults]\n \
 inventory = ~/ansible-SITCON/hosts\n \
 host_key_checking = False\n" > ansible.cfg
 ansible-playbook vlc.yml
